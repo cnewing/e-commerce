ed9a64c9f5d76088b5daeb5efbe3ad28a693c60e
Christa <christanewing47@gmail.com>
2022-08-31 17:15:40 -0400

    populate tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
+
 // The `/api/tags` endpoint
+
 
 router.get('/', (req, res) => {
   // find all tags
@@ -16,6 +18,7 @@ router.get('/', (req, res) => {
       ],
 });
 
+
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   try {
@@ -29,16 +32,51 @@ router.get('/:id', (req, res) => {
       ],
 });
 
+if (!tag_Data) {
+      res.status(404).json({ message: "Cannot find category with this ID!" });
+      return;
+    }
+
+    res.status(200).json(tag_Data);
+  } catch (err) {
+    console.log(ERROR)
+    res.status(500).json(err);
+  }
+});
+
+
 router.post('/', (req, res) => {
   // create a new tag
+try {
+    const tag_Data = await Tag.create(req.body);
+    res.status(200).json(tag_Data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tag_Data = await Tag.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!tag_Data) {
+      res.status(404).json({ message: "Cannot update! No tag forund with this ID!" });
+      return;
+    }
+    res.status(200).json(tag_Data);
+  } catch (err) {
+    console.log(ERROR)
+    res.status(500).json(err);
+  }
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
 });
+
 
 module.exports = router;
